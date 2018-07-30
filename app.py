ae9c465387ab98e82eae1c3541056de1e127f41c
aaronvsg <aaronsalasgranados@mbp-de-aaron.criticalcolocation.net>
2018-07-30 14:04:54 -0600

    1.0
    Nueva versión

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-#!flask/bin/python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 app = Flask(__name__)
 from flask import Flask
 from flask import request
+import requests
+import json
 app = Flask(__name__)
-@app.route('/postjson', methods=['POST'])
-def post():
-    print(request.is_json)
-    content = request.get_json()
-    #print(content)
-    print(content['id'])
-    print(content['name'])
-    return 'JSON posted'
-app.run(host='127.0.0.1', port=5000)
+
+class Authorization(object):
+	def __init__(self, j):
+		self.__dict__ = json.loads(j)
+
+@app.route('/connected', methods=['GET'])
+def connected():
+		response = requests.post('https://test.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9sLbBxQYwWqsS2AAcNaKaVTir.Oy9wS8PRdkyiumXI3Pxw1WUingJioe4mncsDhPh8W3knVN2I2gI2DJY&client_secret=8313047119835055425&username=paleteros@incompany.com.dev&password=Paleter@s2018SHqDtt2lP4BkH1VccRnqRtT0')
+	    	data = response.json()
+	    	print(data)
+	    	j = Authorization(response.text)
+	    	url = 'https://cs14.salesforce.com/services/apexrest/EMONArduino/PlazaRohrmoser-Connected'
+	    	headers = {'Authorization': 'Bearer '+j.access_token}
+	    	response2 = requests.get(url,headers = headers)
+		return response2.text
+
+@app.route('/disconnected', methods=['GET'])
+def disconnected():
+		response = requests.post('https://test.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9sLbBxQYwWqsS2AAcNaKaVTir.Oy9wS8PRdkyiumXI3Pxw1WUingJioe4mncsDhPh8W3knVN2I2gI2DJY&client_secret=8313047119835055425&username=paleteros@incompany.com.dev&password=Paleter@s2018SHqDtt2lP4BkH1VccRnqRtT0')
+	    	data = response.json()
+	    	print(data)
+	    	j = Authorization(response.text)
+	    	url = 'https://cs14.salesforce.com/services/apexrest/EMONArduino/PlazaRohrmoser-Disconnected'
+	    	headers = {'Authorization': 'Bearer '+j.access_token}
+	    	response2 = requests.get(url,headers = headers)
+		return response2.text
+app.run(host='0.0.0.0', port=5000)
